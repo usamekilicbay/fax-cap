86ba01a90d5b1b828f80e6cda8331417850dd853
Üsame <usame.kilicbay@gmail.com>
2023-06-19 23:24:31 +0300

    create: Constants

---
+++ Assets/Scripts/Common/Constants/Key.cs
@@ -0,0 +1,10 @@
+namespace FaxCap.Common.Constant
+{
+    public static class Key
+    {
+        public static class Score
+        {
+            public const string Question = "Question";
+        }
+    }
+}
