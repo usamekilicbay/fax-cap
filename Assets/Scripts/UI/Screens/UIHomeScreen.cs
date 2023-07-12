915c5695b44b8635c2eae572905bc980dd9041a9
Üsame <usame.kilicbay@gmail.com>
2023-07-12 22:12:47 +0300

    update: UIHomeScreen for navigation card approach

--- Assets/Scripts/UI/Screens/UIHomeScreen.cs
+++ Assets/Scripts/UI/Screens/UIHomeScreen.cs
@@ -1,35 +1,62 @@
 using FaxCap.Manager;
+using TMPro;
 using UnityEngine;
-using UnityEngine.UI;
 using Zenject;
 
 namespace FaxCap.UI.Screen
 {
     public class UIHomeScreen : UIScreenBase
     {
-        [SerializeField] private Button startRunButton;
+        [SerializeField] private TextMeshProUGUI leftNavigationText;
+        [SerializeField] private TextMeshProUGUI topNavigationText;
+        [SerializeField] private TextMeshProUGUI rightNavigationText;
+        [SerializeField] private TextMeshProUGUI bottomNavigationText;
 
-        private GameManager _gameManager;
+        private const string _leftNavigationFrontSideName = "";
+        private const string _topNavigationFrontSideName = "";
+        private const string _rightNavigationFrontSideName = "";
+        private const string _bottomNavigationFrontSideName = "";
+        private const string _leftNavigationBackSideName = "";
+        private const string _topNavigationBackSideName = "";
+        private const string _rightNavigationBackSideName = "";
+        private const string _bottomNavigationBackSideName = "";
+
         private UIGameScreen _uiGameScreen;
 
         [Inject]
         public void Construct(GameManager gameManager,
             UIGameScreen gameScreen)
         {
-            _gameManager = gameManager;
             _uiGameScreen = gameScreen;
         }
 
-        private void Awake()
+        public void UpdateNavigationUI(bool isFrontSideActive = true)
         {
-            startRunButton.onClick
-                .AddListener(StartItemCollectRun);
+            UpdateNavigationTexts(isFrontSideActive);
         }
 
-        private void StartItemCollectRun()
+        private void UpdateNavigationTexts(bool isFrontSideActive = true)
         {
-            _gameManager.StartRun();
-            uiManager.ShowScreen(_uiGameScreen);
+            var leftNavigationName = isFrontSideActive
+                ? _leftNavigationFrontSideName
+                : _leftNavigationBackSideName;
+
+            var topNavigationName = isFrontSideActive
+                ? _topNavigationFrontSideName
+                : _topNavigationBackSideName;
+
+            var rightNavigationName = isFrontSideActive
+                ? _rightNavigationFrontSideName
+                : _rightNavigationBackSideName;
+
+            var bottomNavigationName = isFrontSideActive
+                ? _bottomNavigationFrontSideName
+                : _bottomNavigationBackSideName;
+
+            leftNavigationText.SetText(leftNavigationName.ToString());
+            topNavigationText.SetText(topNavigationName.ToString());
+            rightNavigationText.SetText(rightNavigationName.ToString());
+            bottomNavigationText.SetText(bottomNavigationName.ToString());
         }
     }
 }
