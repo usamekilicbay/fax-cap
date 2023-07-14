7fc935bb64ce9c576cca1b7daa40fe9a148e2404
Üsame <usame.kilicbay@gmail.com>
2023-07-14 18:15:21 +0300

    update!: rotation logic for dragging card

--- Assets/Scripts/Card/CardBase.cs
+++ Assets/Scripts/Card/CardBase.cs
@@ -2,8 +2,10 @@ using DG.Tweening;
 using FaxCap.Common.Types;
 using FaxCap.Manager;
 using FaxCap.UI.Screen;
+using System.Collections;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.Experimental.GlobalIllumination;
 using Zenject;
 
 namespace FaxCap.Card
@@ -31,11 +33,22 @@ namespace FaxCap.Card
 
         public CardType CardType;
 
-        public float swipeThreshold = 100f; // Minimum distance to trigger swipe
-        public float rotationFactor = 0.2f; // Rotation factor for swipe effect
-        public float movementSpeed = 5f; // Speed at which the card moves back to the center
-        public float maxRotationAngle = 30f; // Maximum rotation angle
-        public Vector2 movementRange = new(-200f, 200f); // Movement range limits
+        private readonly float _swipeThreshold = 100f; // Minimum distance to trigger swipe
+        private readonly float _movementThreshold = 50f; // Minimum distance to move the card
+        private readonly float _rotationFactor = 0.2f; // Rotation factor for swipe effect
+        private readonly float _movementSpeed = 5f; // Speed at which the card moves back to the center
+        private readonly float _maxRotationAngle = 30f; // Maximum rotation angle
+        private readonly Range _horizontalMovementRange = new() { Min = -180f, Max = 180f }; // Movement range limits
+        private readonly Range _verticalMovementRange = new() { Min = -500f, Max = 500f }; // Movement range limits
+        private readonly float _rotationSpeed = 5f; // Adjust the rotation speed as needed
+        private bool _isHorizontalDrag = false; // Flag to lock the initial movement direction
+        private bool _isHorizontalDragLocked = false; // Flag to lock the initial movement direction
+
+        public class Range
+        {
+            public float Min { get; set; }
+            public float Max { get; set; }
+        }
 
         public bool IsUsed { get; protected set; }
 
@@ -70,79 +83,119 @@ namespace FaxCap.Card
 
         protected virtual void Update()
         {
-            if (isDone)
-                return;
-
-            // If the card is not being dragged and it's not in the initial position,
-            // gradually move it back to the center position
-            //if (!isDragging && cardTransform.anchoredPosition != initialPosition)
-            //{
-            //    cardTransform.anchoredPosition = Vector2.Lerp(cardTransform.anchoredPosition, initialPosition, movementSpeed * Time.deltaTime);
-            //    cardTransform.rotation = Quaternion.Lerp(cardTransform.rotation, initialRotation, movementSpeed * Time.deltaTime);
-            //}
+            // ...
         }
 
         public void OnDrag(PointerEventData eventData)
         {
             // Calculate the distance of the drag
-            float dragDistance = eventData.position.x - eventData.pressPosition.x + initialPosition.x;
-            //float dragDistance = eventData.position.x - eventData.pressPosition.x;
+            float dragDistanceX = eventData.position.x - eventData.pressPosition.x;
+            float dragDistanceY = eventData.position.y - eventData.pressPosition.y;
 
-            // Apply the rotation effect based on the drag distance, even if it's below the rotation threshold
-            float rotationAngle = Mathf.Clamp(dragDistance * rotationFactor, -maxRotationAngle, maxRotationAngle);
-            cardTransform.rotation = initialRotation * Quaternion.Euler(0f, 0f, -rotationAngle);
+            // Determine the dominant axis based on the absolute drag distances
+            bool isHorizontalDrag = Mathf.Abs(dragDistanceX) >= Mathf.Abs(dragDistanceY);
+
+            // Check if dragging has started in the current direction
+            if (!isDragging)
+            {
+                isDragging = true;
+                _isHorizontalDragLocked = isHorizontalDrag; // Lock the direction of movement
+            }
 
             // Move the card horizontally based on the drag distance, within the movement range limits
-            float targetX = Mathf.Clamp(initialPosition.x + dragDistance, movementRange.x, movementRange.y);
-            cardTransform.anchoredPosition = new Vector2(targetX, initialPosition.y);
+            if (_isHorizontalDragLocked)
+            {
+                float targetX = Mathf.Clamp(initialPosition.x + dragDistanceX, _horizontalMovementRange.Min, _horizontalMovementRange.Max);
+                cardTransform.anchoredPosition = new Vector2(targetX, initialPosition.y);
+
+                // Rotate back to initial rotation and lean to the left side if moving towards the right beyond the initial position
+                if (targetX > initialPosition.x)
+                {
+                    float leanRotationAngle = Mathf.Lerp(0f, -_maxRotationAngle, Mathf.InverseLerp(initialPosition.x, _horizontalMovementRange.Max, targetX));
+                    cardTransform.rotation = Quaternion.Euler(0f, 0f, leanRotationAngle);
+                }
+                else if (targetX < initialPosition.x)
+                {
+                    float leanRotationAngle = Mathf.Lerp(0f, _maxRotationAngle, Mathf.InverseLerp(initialPosition.x, _horizontalMovementRange.Min, targetX));
+                    cardTransform.rotation = Quaternion.Euler(0f, 0f, leanRotationAngle);
+                }
+            }
+            // Move the card vertically based on the drag distance
+            else if (!_isHorizontalDragLocked)
+            {
+                float targetY = Mathf.Clamp(initialPosition.y + dragDistanceY, _horizontalMovementRange.Min, _horizontalMovementRange.Max);
+                cardTransform.anchoredPosition = new Vector2(initialPosition.x, targetY);
+                cardTransform.rotation = initialRotation;
+            }
 
             // Calculate the normalized position within the movement range
-            float normalizedPosition = Mathf.InverseLerp(movementRange.x, movementRange.y, targetX);
+            float targetXNormalized = Mathf.InverseLerp(_horizontalMovementRange.Min, _horizontalMovementRange.Max, cardTransform.anchoredPosition.x);
+            float targetYNormalized = Mathf.InverseLerp(_horizontalMovementRange.Min, _horizontalMovementRange.Max, cardTransform.anchoredPosition.y);
 
             // Smoothly interpolate the background color between red (left side), white (midpoint), and green (right side)
-            Color targetColor = Color.Lerp(Color.red, Color.green, normalizedPosition);
-            // Smoothly interpolate to white at the midpoint
-            targetColor = Color.Lerp(gameScreen.BackgroundInitialColor, targetColor, Mathf.Abs(normalizedPosition - 0.5f) * 2f);
+            Color targetColor = Color.Lerp(Color.red, Color.green, _isHorizontalDragLocked ? targetXNormalized : targetYNormalized);
+            targetColor = Color.Lerp(Color.white, targetColor, Mathf.Abs((_isHorizontalDragLocked ? targetXNormalized : targetYNormalized) - 0.5f) * 2f);
             gameScreen.UpdateBackgroundColor(targetColor);
-
-            isDragging = true;
         }
 
         public void OnEndDrag(PointerEventData eventData)
         {
             isDragging = false;
 
-            // Calculate the distance of the drag
-            float dragDistance = eventData.position.x - eventData.pressPosition.x;
+            float posX = cardTransform.anchoredPosition.x;
+            float posY = cardTransform.anchoredPosition.y;
 
-            // If the drag distance does not exceed the threshold, call the reset the card positon and the background color
-            if (Mathf.Abs(dragDistance) < swipeThreshold)
+            // If the drag distance does not exceed the threshold, reset the card position and the background color
+            if (Mathf.Abs(posX) < _swipeThreshold
+                && Mathf.Abs(posY) < _swipeThreshold)
             {
-                // Reset card position and rotation if the drag distance is below the threshold
                 ResetCard();
+                _isHorizontalDragLocked = false; // Reset the direction locking
                 return;
             }
 
-            isDone = true;
-
-            if (dragDistance > 0f)
-                SwipeRight();
+            if (_isHorizontalDragLocked)
+            {
+                if (posX > 0f)
+                    SwipeRight();
+                else if (posX < 0f)
+                    SwipeLeft();
+            }
             else
-                SwipeLeft();
+            {
+                if (posY > 0f)
+                    SwipeUp();
+                else if (posY < 0f)
+                    SwipeDown();
+            }
+
+            // Reset the direction locking
+            _isHorizontalDragLocked = false;
+        }
+
+        private IEnumerator RotateCardToInitialRotation()
+        {
+            Quaternion startRotation = cardTransform.rotation;
+            Quaternion targetRotation = initialRotation;
+
+            float rotationTime = 0f;
+            float rotationDuration = 0.5f; // Adjust the duration as needed
+
+            while (rotationTime < rotationDuration)
+            {
+                rotationTime += Time.deltaTime;
+                float t = rotationTime / rotationDuration;
+                cardTransform.rotation = Quaternion.Lerp(startRotation, targetRotation, t);
+                yield return null;
+            }
+
+            cardTransform.rotation = targetRotation;
         }
 
         #endregion
 
         protected virtual void Setup()
         {
-            //backSide.transform.rotation = Quaternion.Euler(Vector3.up * 180);
-            //_defaultScale = transform.localScale;
-            //transform.localScale = _defaultScale * 0.7f;
-            //transform.DOScale(_defaultScale, 0.5f);
-            //var cloakColor = cloakRenderer.color;
-            //cloakColor.a = 0;
-            //cloakRenderer.color = cloakColor;
-
             cardTransform = GetComponent<RectTransform>();
             initialPosition = cardTransform.anchoredPosition;
             initialRotation = cardTransform.rotation;
