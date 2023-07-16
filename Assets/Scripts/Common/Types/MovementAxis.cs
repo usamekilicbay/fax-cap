fce08fa3e3ef7731bd5ecfd83aeb4e635256c412
Üsame <usame.kilicbay@gmail.com>
2023-07-16 11:41:23 +0300

    update: CardMovementDirection as MovementAxis

---
+++ Assets/Scripts/Common/Types/MovementAxis.cs
@@ -0,0 +1,11 @@
+﻿namespace Assets.Scripts.Common.Types
+{
+    [System.Flags]
+    public enum MovementAxis
+    {
+        Initial = 1,    // Binary: 0001
+        Neutral = 2,    // Binary: 0010
+        Horizontal = 4, // Binary: 0100
+        Vertical = 8    // Binary: 1000
+    }
+}
