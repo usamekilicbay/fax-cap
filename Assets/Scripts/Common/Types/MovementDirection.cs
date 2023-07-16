0cf7f8f015979badb515c4b6c0e9bf8e7dd2816b
Üsame <usame.kilicbay@gmail.com>
2023-07-16 11:42:11 +0300

    create: MovementDirection for allowed movement directions

---
+++ Assets/Scripts/Common/Types/MovementDirection.cs
@@ -0,0 +1,12 @@
+﻿namespace Assets.Scripts.Common.Types
+{
+    [System.Flags]
+    public enum MovementDirection
+    {
+        None = 0,       // Represents no movement direction
+        Left = 1,       // Binary: 0001
+        Right = 2,      // Binary: 0010
+        Top = 4,        // Binary: 0100
+        Bottom = 8      // Binary: 1000
+    }
+}
