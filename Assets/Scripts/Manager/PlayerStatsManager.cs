a3cc316d4123bfacb2661fe59ef8b934dc208c33
Üsame <usame.kilicbay@gmail.com>
2023-07-26 19:02:33 +0300

    create: PlayerStatsManager

---
+++ Assets/Scripts/Manager/PlayerStatsManager.cs
@@ -0,0 +1,35 @@
+﻿using FaxCap.Common.Abstract;
+using FaxCap.UI.Screen;
+using System.Threading.Tasks;
+using Zenject;
+
+namespace FaxCap.Manager
+{
+    public class PlayerStatsManager : ICompletable
+    {
+        private SaveManager _saveManager;
+        private UIResultScreen _resultScreen;
+
+        [Inject]
+        public void Construct(SaveManager saveManager, UIResultScreen resultScreen)
+        {
+            _saveManager = saveManager;
+            _resultScreen = resultScreen;
+        }
+
+        public bool HasExceedBestScore(int currentScore)
+            => currentScore > 0;
+
+        public bool HasExceededBestCombo(int currentCombo)
+            => currentCombo > 0;
+
+        public bool HasExceededBestQuestionCount(int currentQuestionCount)
+            => currentQuestionCount > 0;
+
+        public async Task Complete(bool isSuccessful = true)
+        {
+            await _resultScreen.UpdateQuestionText(13);
+            await _resultScreen.UpdateComboText(4);
+        }
+    }
+}
