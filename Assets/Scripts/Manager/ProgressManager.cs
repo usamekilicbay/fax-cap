645aa7d753bd9d119d36740fef03341e56b3e8f8
Üsame <usame.kilicbay@gmail.com>
2023-07-26 19:11:31 +0300

    update: implement IRenevable and ICompletable to ProgressManager

--- Assets/Scripts/Manager/ProgressManager.cs
+++ Assets/Scripts/Manager/ProgressManager.cs
@@ -1,42 +1,57 @@
+using FaxCap.Common.Abstract;
 using FaxCap.Common.Constant;
 using FaxCap.UI.Screen;
+using System.Threading.Tasks;
 using Zenject;
 
 namespace FaxCap.Manager
 {
-    public class ProgressManager
+    public class ProgressManager : IRenewable, ICompletable
     {
         private int _progress;
 
         private UIGameScreen _gameScreen;
+        private UIResultScreen _resultScreen;
         private ConfigurationManager _configurationManager;
 
         [Inject]
         public void Construct(UIGameScreen gameScreen,
+            UIResultScreen resultScreen,
             ConfigurationManager configurationManager)
         {
             _gameScreen = gameScreen;
+            _resultScreen = resultScreen;
             _configurationManager = configurationManager;
         }
 
         public async void Progress()
         {
             _progress++;
-            await _gameScreen.UpdateProgressBar(_progress);
+            _gameScreen.UpdateProgressBar(_progress);
 
             if (HasReachedToMilestone())
-            {
                 Milestone();
-                await _gameScreen.UpdateProgressBar(_progress);
-            }
+        }
+
+        public int RemainingToMilestone()
+            => _configurationManager.GameConfigs.ProgressMilestone - _progress;
+
+        public bool HasReachedToMilestone()
+           => _progress == _configurationManager.GameConfigs.ProgressMilestone;
+
+        public async Task Complete(bool isSuccessful = true)
+        {
+            _progress = 0;
+        }
+
+        public void Renew()
+        {
+            _progress = 0;
         }
 
         private void Milestone()
         {
             _progress = 0;
         }
-
-        public bool HasReachedToMilestone()
-            => _progress == _configurationManager.GameConfigs.ProgressMilestone;
     }
 }
