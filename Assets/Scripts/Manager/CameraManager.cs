947c45df0571050251957045b041ed0610d50906
Üsame <usame.kilicbay@gmail.com>
2023-07-26 19:01:46 +0300

    create: CameraManager

---
+++ Assets/Scripts/Manager/CameraManager.cs
@@ -0,0 +1,26 @@
+﻿using DG.Tweening;
+using UnityEngine;
+
+namespace FaxCap.Manager
+{
+    public class CameraManager : MonoBehaviour
+    {
+        [SerializeField]
+        [Tooltip("Reference to the UI root's RectTransform.")]
+        private RectTransform uiRootTransform;
+
+        [SerializeField]
+        [Tooltip("Duration of the shake effect in seconds.")]
+        private float duration = 0.5f;
+
+        [SerializeField]
+        [Tooltip("Strength of the shake effect.")]
+        private float strength = 1f;
+
+
+        public void ShakeCamera()
+        {
+            uiRootTransform.DOShakeAnchorPos(duration, strength);
+        }
+    }
+}
