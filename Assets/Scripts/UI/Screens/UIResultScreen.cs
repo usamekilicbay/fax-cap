a71f118507f1249dcbf2d818af8994579dc287dd
Üsame <usame.kilicbay@gmail.com>
2023-07-26 19:17:22 +0300

    update!: result animations in UIResultScreen

--- Assets/Scripts/UI/Screens/UIResultScreen.cs
+++ Assets/Scripts/UI/Screens/UIResultScreen.cs
@@ -1,3 +1,4 @@
+using Assets.Scripts.Common.Animations;
 using DG.Tweening;
 using FaxCap.Common.Abstract;
 using FaxCap.Manager;
@@ -38,19 +39,30 @@ namespace FaxCap.UI.Screen
 
         private RectTransform _scoreRectTransform;
         private Vector2 _scoreTextInitialSize;
+        private Vector2 _questionCountTextInitialSize;
+        private Vector2 _comboTextInitialSize;
 
         private GameManager _gameManager;
         private UIHomeScreen _uiHomeScreen;
         private UIGameScreen _uiGameScreen;
+        private ScoreManager _scoreManager;
+        private PlayerStatsManager _playerStatsManager;
+        private LevelManager _levelManager;
 
         [Inject]
         public void Construct(GameManager gameManager,
             UIHomeScreen homeScreen,
-            UIGameScreen gameScreen)
+            UIGameScreen gameScreen,
+            ScoreManager scoreManager,
+            PlayerStatsManager playerStatsManager,
+            LevelManager levelManager)
         {
             _gameManager = gameManager;
             _uiGameScreen = gameScreen;
             _uiHomeScreen = homeScreen;
+            _scoreManager = scoreManager;
+            _playerStatsManager = playerStatsManager;
+            _levelManager = levelManager;
         }
 
         private void Awake()
@@ -58,13 +70,19 @@ namespace FaxCap.UI.Screen
             homeButton.onClick
                 .AddListener(GoToHomeScreen);
 
-            _scoreRectTransform = scoreText.transform.parent.GetComponent<RectTransform>();
-            _scoreTextInitialSize = scoreText.rectTransform.sizeDelta;
+            Setup();
+
+            //StartTheDance();
         }
 
-        public void UpdateQuestionText(int questionCount)
+        public async Task UpdateQuestionText(int questionCount)
         {
-            questionCountText.SetText($"Questions: {questionCount}");
+            ToggleQuestionCounterBestLabel(_playerStatsManager.HasExceededBestQuestionCount(questionCount));
+
+            await questionCountText
+                .AnimateTextt(0, 20, _questionCountTextInitialSize, _questionCountTextInitialSize * 2f, 1f);
+
+            //questionCountText.SetText($"Questions: {questionCount}");
         }
 
         public void UpdateScoreText(int score)
@@ -78,70 +96,106 @@ namespace FaxCap.UI.Screen
 
             foreach (var score in scores)
             {
-                //await GenerateScoreTextAsync(score);
-                GenerateScoreTextAsync(score);
+                //GenerateScoreTextAsync(score);
+                await GenerateScoreTextAsync(score);
                 var currentScore = totalScore;
                 totalScore += score;
 
-                await AnimateScore(currentScore, totalScore);
+                await scoreText.AnimateTextt(currentScore, totalScore, _scoreTextInitialSize,
+                    _scoreTextInitialSize * 2f, 0.3f);
             }
         }
 
-        public void ShowScoreBestLabel()
-        {
-            scoreBestLabel.enabled = true;
-        }
-
-        public void ShowQuestionCounterBestLabel()
+        public void UpdateBestScoreText(int bestScore)
         {
             questionCounterBestLabel.enabled = true;
-        }
-
-        public void ShowComboBestLabel()
-        {
-            comboBestLabel.enabled = true;
-        }
-
-        private async Task AnimateScore(int currentScore, int targetScore)
-        {
-            var duration = 0.3f;
-
-            var scoreUpdateTween = DOTween.To(() => currentScore, x => currentScore = x, targetScore, duration);
-            scoreUpdateTween.SetEase(Ease.Linear);
-            scoreUpdateTween.Pause();
-
-            var targetSize = _scoreTextInitialSize * 2f;
-
-            var scoreSizeTween = scoreText.rectTransform.DOSizeDelta(targetSize, duration);
-            scoreSizeTween.SetEase(Ease.InBack);
-            scoreSizeTween.SetLoops(2, LoopType.Yoyo);
-            scoreSizeTween.Pause();
-
-            var sequence = DOTween.Sequence();
-            sequence.Append(scoreUpdateTween);
-            sequence.Join(scoreSizeTween);
-            sequence.OnUpdate(() => scoreText.SetText(currentScore.ToString()));
-            sequence.OnComplete(() => scoreText.SetText(targetScore.ToString()));
-            sequence.Restart();
-
-            await sequence.AsyncWaitForCompletion();
-        }
-
-        public void UpdateHighScoreText(int highScore)
-        {
-            questionCounterBestLabel.enabled = true;
-            questionCounterBestLabel.SetText($"High Score");
+            questionCounterBestLabel.SetText($"Best Score");
             //highScoreText.SetText($"High Score: {highScore}");
         }
 
-        public void UpdateComboText(int combo)
+        public async Task UpdateComboText(int comboCount)
         {
-            comboText.SetText($"{combo}");
+            ToggleComboBestLabel(_playerStatsManager.HasExceededBestCombo(comboCount));
+
+            await comboText.AnimateTextt(0, comboCount, _comboTextInitialSize, _comboTextInitialSize * 2f, 0.3f);
+
+            //comboText.SetText($"{combo}");
         }
 
-        public async void UpdateLevel(float exp, int currentLevel)
+        public async Task UpdateLevel(float exp, int currentLevel)
         {
+            expBar.value = 0f;
+            expBar.maxValue = _levelManager.GetRequiredExp(currentLevel);
+            UpdateLevelTexts(currentLevel - 1);
             await UpdateExpBar(exp, currentLevel);
+        }
+
+        public void UpdateLevelTexts(int currentLevel)
+        {
+            currentLevelText.SetText($"Level: {currentLevel}");
+            nextLevelText.SetText($"Level: {currentLevel + 1}");
+        }
+
+        public async Task GenerateScoreTextAsync(int score)
+        //public void GenerateScoreTextAsync(int score)
+        {
+            ToggleScoreBestLabel(_playerStatsManager.HasExceedBestScore(1));
+
+            var duration = 0.5f;
+
+            var targetPos = scoreText.rectTransform.position;
+            var leftBorder = scoreText.rectTransform.rect.xMin;
+            var rightBorder = scoreText.rectTransform.rect.xMax;
+
+            var spawnPos = new Vector2
+            {
+                x = Random.Range(leftBorder, rightBorder),
+                y = 0f
+            };
+
+            var scoreAnimationText = Instantiate(scoreAnimationTextPrefab, scoreAnimationTextSpawnPoint)
+                .GetComponent<TextMeshProUGUI>();
+
+            scoreAnimationText.rectTransform.anchoredPosition = spawnPos;
+
+            scoreAnimationText.SetText(score.ToString());
+
+            var sequence = DOTween.Sequence();
+            sequence.Append(scoreAnimationText.rectTransform.DOMoveY(targetPos.y, duration));
+            sequence.Join(scoreAnimationText.DOFade(0, duration));
+            await sequence.AsyncWaitForCompletion();
+        }
+
+        public override Task Show()
+        {
+            return base.Show();
+        }
+
+        public override Task Hide()
+        {
+            Renew();
+
+            return base.Hide();
+        }
+
+        public void Renew()
+        {
+            questionCounterBestLabel.enabled = false;
+
+            UpdateLevel(_levelManager.Exp, _levelManager.Level);
+        }
+
+        private void Setup()
+        {
+            _scoreRectTransform = scoreText.transform.parent.GetComponent<RectTransform>();
+            _scoreTextInitialSize = scoreText.rectTransform.sizeDelta;
+            _questionCountTextInitialSize = questionCountText.rectTransform.sizeDelta;
+            _comboTextInitialSize = comboText.rectTransform.sizeDelta;
+        }
+
+        private void GoToHomeScreen()
+        {
+            uiManager.ShowScreen(_uiHomeScreen);
         }
 
         private async Task UpdateExpBar(float exp, int currentLevel)
@@ -181,60 +235,19 @@ namespace FaxCap.UI.Screen
             expText.SetText($"{expBar.value}/{expBar.maxValue}");
         }
 
-        public void UpdateLevelTexts(int currentLevel)
+        private void ToggleScoreBestLabel(bool isEnabled)
         {
-            currentLevelText.SetText($"Level: {currentLevel}");
-            nextLevelText.SetText($"Level: {currentLevel + 1}");
+            scoreBestLabel.enabled = isEnabled;
         }
 
-        //public async Task GenerateScoreTextAsync(int score)
-        public void GenerateScoreTextAsync(int score)
+        private void ToggleQuestionCounterBestLabel(bool isEnabled)
         {
-            var duration = 0.5f;
-
-            var targetPos = scoreText.rectTransform.position;
-            var leftBorder = scoreText.rectTransform.rect.xMin;
-            var rightBorder = scoreText.rectTransform.rect.xMax;
-
-            var spawnPos = new Vector2
-            {
-                x = Random.Range(leftBorder, rightBorder),
-                y = 0f
-            };
-
-            var scoreAnimationText = Instantiate(scoreAnimationTextPrefab, scoreAnimationTextSpawnPoint)
-                .GetComponent<TextMeshProUGUI>();
-
-            scoreAnimationText.rectTransform.anchoredPosition = spawnPos;
-
-            scoreAnimationText.SetText(score.ToString());
-
-            var sequence = DOTween.Sequence();
-            sequence.Append(scoreAnimationText.rectTransform.DOMoveY(targetPos.y, duration));
-            sequence.Join(scoreAnimationText.DOFade(0, duration));
-            //await sequence.AsyncWaitForCompletion();
+            questionCounterBestLabel.enabled = isEnabled;
         }
 
-        private void GoToHomeScreen()
+        private void ToggleComboBestLabel(bool isEnabled)
         {
-            uiManager.ShowScreen(_uiHomeScreen);
-        }
-
-        public override Task Show()
-        {
-            return base.Show();
-        }
-
-        public override Task Hide()
-        {
-            Renew();
-
-            return base.Hide();
-        }
-
-        public void Renew()
-        {
-            questionCounterBestLabel.enabled = false;
+            comboBestLabel.enabled = isEnabled;
         }
     }
 }
