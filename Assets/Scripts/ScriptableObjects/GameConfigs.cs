d54526f6e6932dc5763e3647187147dd06f8c5fc
Üsame <usame.kilicbay@gmail.com>
2023-07-26 19:03:56 +0300

    add: tooltips and comment lines to GameConfigs

--- Assets/Scripts/ScriptableObjects/GameConfigs.cs
+++ Assets/Scripts/ScriptableObjects/GameConfigs.cs
@@ -5,8 +5,16 @@ namespace FaxCap.Configs
     [CreateAssetMenu(fileName = "GameConfigs", menuName = "Configs/Game Configs")]
     public class GameConfigs : ScriptableObject
     {
+        [Tooltip("The default time period (in seconds) given to answer a question.")]
+        [SerializeField] private float defaultAnswerPeriod;
+
+        [Tooltip("The milestone value at which the player's progress is updated.")]
         [SerializeField] private int progressMilestone;
 
+        // Getter property for the default answer period.
+        public float DefaultAnswerPeriod => defaultAnswerPeriod;
+
+        // Getter property for the progress milestone.
         public int ProgressMilestone => progressMilestone;
     }
 }
